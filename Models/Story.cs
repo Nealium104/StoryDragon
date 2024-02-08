d1814542898251fa9d2acee524f20f17685a0c06
Neal Grindstaff <neal.grindstaff@gmail.com>
2024-02-08 08:41:55 -0500

    added story class

---
+++ Models/Story.cs
@@ -0,0 +1,10 @@
+﻿namespace StoryDragon.Models
+{
+    public class Story
+    {
+        public Guid StoryID { get; set; } = Guid.NewGuid();
+        public string StoryTitle { get; set; }
+        public string StoryDescription { get; set; }
+        public string StoryText { get; set; }
+    }
+}
